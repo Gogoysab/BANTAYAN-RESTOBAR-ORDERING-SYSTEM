1e2699e7f63aa7f97fcff3609ff271f9c91a1684
Gogoysab <128393711+Gogoysab@users.noreply.github.com>
2024-08-06 07:59:04 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -251,7 +251,7 @@ if (isset($_POST['signup'])) {
                 <input type="text" id="name" name="name" class="form-control my-2" required>
             </div>
             <div class="form-group">
-                <label for="email">Email</label>
+                <label for="email">Email1</label>
                 <input type="email" id="email" name="email" class="form-control my-2" required>
             </div>
             <div class="form-group">
